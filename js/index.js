4791121f8cd2f88d04007ba2518b95562909b762
Yaswanth-A1234 <akasamyaswanth1@gmail.com>
2023-12-20 15:38:38 +0530

    added the joystick for small devices

---
+++ js/index.js
@@ -0,0 +1,289 @@
+// Game Constants & Variables
+let inputDir = {x: 0, y: 0}; 
+const foodSound = new Audio('music/food.mp3');
+const gameOverSound = new Audio('music/gameover.mp3');
+const moveSound = new Audio('music/move.mp3');
+const musicSound = new Audio('music/music.mp3');
+let speed = 13;
+let score = 0;
+let lastPaintTime = 0;
+let snakeArr = [
+    {x: 13, y: 15}
+];
+
+food = {x: 6, y: 7};
+
+// Game Functions
+function main(ctime) {
+    window.requestAnimationFrame(main);
+    // console.log(ctime)
+    if((ctime - lastPaintTime)/1000 < 1/speed){
+        return;
+    }
+    lastPaintTime = ctime;
+    gameEngine();
+}
+
+function isCollide(snake) {
+    // If you bump into yourself 
+    for (let i = 1; i < snakeArr.length; i++) {
+        if(snake[i].x === snake[0].x && snake[i].y === snake[0].y){
+            return true;
+        }
+    }
+    // If you bump into the wall
+    if(snake[0].x >= 18 || snake[0].x <=0 || snake[0].y >= 18 || snake[0].y <=0){
+        return true;
+    }
+        
+    return false;
+}
+
+function gameEngine(){
+    // Part 1: Updating the snake array & Food
+    if(isCollide(snakeArr)){
+        gameOverSound.play();
+        musicSound.pause();
+        inputDir =  {x: 0, y: 0}; 
+        alert("Game Over. Press any key to play again!");
+        snakeArr = [{x: 13, y: 15}];
+        musicSound.play();
+        score = 0; 
+    }
+
+    // If you have eaten the food, increment the score and regenerate the food
+    if(snakeArr[0].y === food.y && snakeArr[0].x ===food.x){
+        foodSound.play();
+        score += 1;
+        if(score>hiscoreval){
+            hiscoreval = score;
+            localStorage.setItem("hiscore", JSON.stringify(hiscoreval));
+            hiscoreBox.innerHTML = "HiScore: " + hiscoreval;
+        }
+        scoreBox.innerHTML = "Score: " + score;
+        snakeArr.unshift({x: snakeArr[0].x + inputDir.x, y: snakeArr[0].y + inputDir.y});
+        let a = 2;
+        let b = 16;
+        food = {x: Math.round(a + (b-a)* Math.random()), y: Math.round(a + (b-a)* Math.random())}
+    }
+
+    // Moving the snake
+    for (let i = snakeArr.length - 2; i>=0; i--) { 
+        snakeArr[i+1] = {...snakeArr[i]};
+    }
+
+    snakeArr[0].x += inputDir.x;
+    snakeArr[0].y += inputDir.y;
+
+    // Part 2: Display the snake and Food
+    // Display the snake
+    board.innerHTML = "";
+    snakeArr.forEach((e, index)=>{
+        snakeElement = document.createElement('div');
+        snakeElement.style.gridRowStart = e.y;
+        snakeElement.style.gridColumnStart = e.x;
+
+        if(index === 0){
+            snakeElement.classList.add('head');
+        }
+        else{
+            snakeElement.classList.add('snake');
+        }
+        board.appendChild(snakeElement);
+    });
+    // Display the food
+    foodElement = document.createElement('div');
+    foodElement.style.gridRowStart = food.y;
+    foodElement.style.gridColumnStart = food.x;
+    foodElement.classList.add('food')
+    board.appendChild(foodElement);
+
+
+}
+
+
+// Main logic starts here
+musicSound.play();
+let hiscore = localStorage.getItem("hiscore");
+if(hiscore === null){
+    hiscoreval = 0;
+    localStorage.setItem("hiscore", JSON.stringify(hiscoreval))
+}
+else{
+    hiscoreval = JSON.parse(hiscore);
+    hiscoreBox.innerHTML = "HiScore: " + hiscore;
+}
+
+// window.requestAnimationFrame(main);
+
+// const upButton = document.querySelector('.up-button');
+// const downButton = document.querySelector('.down-button');
+// const leftButton = document.querySelector('.left-button');
+// const rightButton = document.querySelector('.right-button');
+
+
+
+// window.addEventListener('keydown', e =>{
+//     inputDir = {x: 0, y: 1} // Start the game
+//     moveSound.play();
+//     switch (e.key) {
+//         case "w":
+//             console.log("ArrowUp");
+//             inputDir.x = 0;
+//             inputDir.y = -1;
+//             break;
+        
+
+//         case "s":
+//             console.log("ArrowDown");
+//             inputDir.x = 0;
+//             inputDir.y = 1;
+//             break;
+
+//         case "a":
+//             console.log("ArrowLeft");
+//             inputDir.x = -1;
+//             inputDir.y = 0;
+//             break;
+
+//         case "d":
+//             console.log("ArrowRight");
+//             inputDir.x = 1;
+//             inputDir.y = 0;
+//             break;
+//         default:
+//             break;
+//     }
+
+// });
+
+window.requestAnimationFrame(main);
+
+const upButton = document.querySelector('.up-button');
+const downButton = document.querySelector('.down-button');
+const leftButton = document.querySelector('.left-button');
+const rightButton = document.querySelector('.right-button');
+
+// Function to handle button clicks
+function handleButtonClick(direction) {
+    inputDir = { x: 0, y: 1 }; // Start the game
+    moveSound.play();
+
+    switch (direction) {
+        case "up":
+            console.log("ArrowUp");
+            inputDir.x = 0;
+            inputDir.y = -1;
+            break;
+
+        case "down":
+            console.log("ArrowDown");
+            inputDir.x = 0;
+            inputDir.y = 1;
+            break;
+
+        case "left":
+            console.log("ArrowLeft");
+            inputDir.x = -1;
+            inputDir.y = 0;
+            break;
+
+        case "right":
+            console.log("ArrowRight");
+            inputDir.x = 1;
+            inputDir.y = 0;
+            break;
+
+        default:
+            break;
+    }
+}
+
+// Add click event listeners to the buttons
+upButton.addEventListener('click', () => handleButtonClick("up"));
+downButton.addEventListener('click', () => handleButtonClick("down"));
+leftButton.addEventListener('click', () => handleButtonClick("left"));
+rightButton.addEventListener('click', () => handleButtonClick("right"));
+
+// Add keyboard event listener
+window.addEventListener('keydown', e => {
+    // Handle keyboard events (similar to your existing code)
+    inputDir = { x: 0, y: 1 }; // Start the game
+    moveSound.play();
+
+    switch (e.key) {
+        case "w":
+            handleButtonClick("up");
+            break;
+
+        case "s":
+            handleButtonClick("down");
+            break;
+
+        case "a":
+            handleButtonClick("left");
+            break;
+
+        case "d":
+            handleButtonClick("right");
+            break;
+
+        default:
+            break;
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// upbutton.addEventListener('click', () => {
+//     console.log('upButton clicked!'); // Example action
+//     inputDir.x = 0;
+//     inputDir.y = -1;
+// });
+// downbutton.addEventListener('click', () => {
+//     console.log('downButton clicked!');
+//     inputDir.x = 0;
+//     inputDir.y = 1; // Example action
+//   });
+// rightbutton.addEventListener('click', () => {
+//     console.log('rightButton clicked!'); // Example action
+//     inputDir.x = 1;
+//     inputDir.y = 0;
+//   });
+// leftbutton.addEventListener('click', () => {
+//     console.log('leftButton clicked!');
+//     inputDir.x = -1;
+//     inputDir.y = 0; // Example action
+// });
